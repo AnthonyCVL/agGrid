6b4c08d44e47263b48a30279debb83dbcc40ac8e
anthony.villacorta <anthony.villacorta@telefonica.com>
2022-09-14 12:36:23 -0500

    FIX: Update endpoint

--- src/App.js
+++ src/App.js
@@ -25,15 +25,10 @@ function App() {
   }
 
   const changeTab = (numberTab) => {
-    console.log("changeTab")
-    console.log(activeTab)
-    console.log(numberTab)
     if(activeTab !== numberTab){
       setActiveTab(numberTab)
       setRows(datatables[numberTab-1])
       setColumns(datatablesColumns[numberTab-1])
-      console.log(datatables[numberTab-1])
-      console.log(datatablesColumns[numberTab-1])
     }
   }
 
@@ -50,39 +45,9 @@ function App() {
   ];
 
   const handlerTable = function (e) {
-    console.log("handlerTable")
     const option = JSON.parse(e.target.value);
     setTableSelected(option)
   }
-
-/*const customToolTip = (props) => {
-    const data = useMemo(
-      () => props.api.getDisplayedRowAtIndex(props.rowIndex).data,
-      []
-    );
-  
-    return (
-      <div
-        className="custom-tooltip"
-        style={{ backgroundColor: props.color || 'white' }}
-      >
-        <p>
-          <span>{data.num_row}</span>
-        </p>
-        <p>
-          <span>Country: </span> {data.last_load}
-        </p>
-        <p>
-          <span>Total: </span> {data.val_max}
-        </p>
-      </div>
-    );
-  };
-*/
-  // const columns = [{ headerName: "Make", field: "make" },
-  // { headerName: "Price", field: "price" },
-  // { headerName: "Model", field: "model" },
-  // { headerName: "Date", field: "date" }]
 
   const columnDefs = (key) => ({
       field: key,
@@ -111,21 +76,6 @@ function App() {
         return style;
       }
   })
-
-  const columnDefs_columns = (key) => ({
-    field: key,
-    hide: key.toLowerCase()==='v_obsv' || key.toLowerCase()==='valid_column' || key.toLowerCase()==='obsv' || key.toLowerCase()==='color',
-    cellStyle: params => { 
-      let style = null;
-      if(params.data.color !== null && params.data.color !== "" ){
-        if(key.toLowerCase() === params.data.valid_column){
-          style = { background: params.data.color , color: 'white'}
-        }
-      }
-      console.log("style: "+style)
-      return style;
-    }
-})
 
   const getDynamicColumns = (obj) => {
     return Object.keys(obj).map(key => columnDefs(key))
@@ -165,26 +115,9 @@ function App() {
     //minWidth: 100
   }
 
-
-  const showTableData2 = async () => {
-    try {
-      //const base_url='http://localhost:8080'
-      const base_url='http://ms-python-teradata-git-nirvana-qa.apps.ocptest.gp.inet'
-      const params_group='/getTableData?database=D_EWAYA_CONFIG&table='+tableSelected.table_name
-      const response_group=await fetch(base_url+params_group)
-      const response = await fetch(base_url+'/getTableData?database=D_EWAYA_CONFIG&table='+tableSelected.table_name+'&select='+tableSelected.col_qry+'&order='+tableSelected.ord_qry);
-      const data = await response.json();
-      console.log(data)
-      setRows(data)
-      setColumns(getDynamicColumns(data[0]))
-    } catch (error) {
-      console.error("There has been a problem with your fetch operation:", error);
-    }
-  }
-
   const request_gettabledata = async (body) => {
-    const base_url='http://localhost:8080'
-    //const base_url='http://ms-python-teradata-git-nirvana-qa.apps.ocptest.gp.inet'
+    //const base_url='http://localhost:8080'
+    const base_url='http://ms-python-teradata-nirvana-qa.apps.ocptest.gp.inet'
     const method = '/getTableData2'
     const request = {
       method: 'POST',
@@ -200,29 +133,8 @@ function App() {
     return json
   }
 
-  const request_gettabledata2 = async (body) => {
-    const base_url='http://localhost:8080'
-    //const base_url='http://ms-python-teradata-git-nirvana-qa.apps.ocptest.gp.inet'
-    const method = '/getTableData2'
-    const request = {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: body
-    };
-    return await send_post2(base_url, method, request)
-  }
-
-  const send_post2 = async (base_url, method, request) => {
-    return fetch(base_url+method, request)
-     
-  }
-
   const showTableData = async () => {
-    console.log("showTableData async")
-    console.log(groupTables)
     try { 
-      console.log("tableSelected.id")
-      console.log(tableSelected.id)
       if(tableSelected.id<=0 || tableSelected.id==undefined){
         return;
       }
@@ -234,8 +146,6 @@ function App() {
           })
         )
       
-      //setRows(group_tables)
-    console.log(groupTables)
       setGroupTables(group_tables.sort((a, b) => a.position_table > b.position_table ? 1 : -1))
       const dts = []
       const promises=[]
@@ -246,7 +156,6 @@ function App() {
       });
 
       const resultados = await Promise.all( promises.map( function(key) {
-        console.log(key)
         const tables =  request_gettabledata( 
           JSON.stringify({ 
             database: 'D_EWAYA_CONFIG',
@@ -289,64 +198,10 @@ function App() {
     }
   }
 
-  const showTableData3 = async () => {
-    try { 
-      const group_tables = await request_gettabledata( 
-        JSON.stringify({ 
-          database: 'D_EWAYA_CONFIG',
-          table: 'TB_CONFIG_FE_GROUP_TABLE',
-          where: JSON.stringify({ id_group: tableSelected.id, state: 1 })
-        })
-      )
-      console.log("group_tables")
-      console.log(group_tables)
-      setRows(group_tables)
-      setGroupTables(group_tables)
-      const dts = []
-
-      Object.keys(group_tables).forEach(async function(key) {
-        const tables = await request_gettabledata( 
-          JSON.stringify({ 
-            database: 'D_EWAYA_CONFIG',
-            table: 'TB_CONFIG_FE',
-            where: JSON.stringify({ id: group_tables[key].id_table, state: 1})
-          })
-        )
-        console.log("TABLES")
-        console.log(tables)
-        const table = tables[0]
-        console.log("TABLE")
-        console.log(table)
-        const dt = await request_gettabledata( 
-          JSON.stringify({ 
-            database: table.database_name,
-            table: table.table_name,
-            select: table.col_qry,
-            order: table.ord_qry
-          })
-        )
-        //dts.push(dt)
-        addElementToArray(dts, dt)
-        console.log("dt")
-        console.log(dt)
-      });
-      console.log("dts")
-      console.log(dts)
-      setDatatables(dts)
-      console.log("DATATABLESSSSSSSSSSSSSSSSSSSSSSSSSSSS")
-      console.log(datatables)
-      //const response = await fetch(base_url+'/getTableData?database=D_EWAYA_CONFIG&table='+tableSelected.table_name+'&select='+tableSelected.col_qry+'&order='+tableSelected.ord_qry);
-      //const data = await response.json();
-      setColumns(getDynamicColumns(group_tables[0]))
-    } catch (error) {
-      console.error("There has been a problem with your fetch operation:", error);
-    }
-  }
-
   const showTables = async () => {
     try {
-      const response = await fetch('http://localhost:8080/getTableData?database=D_EWAYA_CONFIG&table=TB_CONFIG_FE_GROUP');
-      //const response = await fetch('http://ms-python-teradata-git-nirvana-qa.apps.ocptest.gp.inet/getTableData?database=D_EWAYA_CONFIG&table=TB_CONFIG_FE_GROUP');
+      //const response = await fetch('http://localhost:8080/getTableData?database=D_EWAYA_CONFIG&table=TB_CONFIG_FE_GROUP');
+      const response = await fetch('http://ms-python-teradata-nirvana-qa.apps.ocptest.gp.inet/getTableData?database=D_EWAYA_CONFIG&table=TB_CONFIG_FE_GROUP');
       const data = await response.json();
       setRowTables(data)
       setTableSelected(data[0])
@@ -357,32 +212,18 @@ function App() {
   }
 
   useEffect(()=>{ 
-    console.log("showTables")
     showTables()
   }, [])
   
   useEffect(()=>{ 
-    console.log("showTableData")
     showTableData()
     setActiveTab(0)
     //changeTab(1)
   }, [tableSelected])
 
-  /*useEffect(()=>{ 
-    console.log("handlerTable")
-    setTableSelected(option)
-  }, [defaultOption])*/
-
   useEffect(()=>{ 
-    console.log("datatablesColumns updated")
     changeTab(1)
   }, [datatablesColumns])
-
-  
-  const cellColor = (obj) => {
-    console.log(obj)
-    return Object.keys(obj).map(key => ({ field: key }))
-  }
 
 /*
   const onGridReady = async (params) => {
@@ -439,16 +280,6 @@ function onFirstDataRendered (params) {
             </TabPane>
           ))}
         </TabContent>
-      </div>
-      <div className="App-datatable ag-theme-alpine" hidden="hidden">
-        <AgGridReact
-          rowData={rows}
-          columnDefs={columns}
-          defaultColDef={defColumnDefs}
-          pagination={true}
-          paginationPageSize={20}
-          //onGridReady={onGridReady} 
-          />
       </div>
     </div>
   );
