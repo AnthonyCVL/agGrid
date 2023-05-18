bc27dc3196583d4d33e81957ac49aabfc7a09669
anthony0950cvl2 <anthony0950cvl2@gmail.com>
2023-05-18 12:58:01 -0500

    FIX: Change URL

--- src/Tablerow.js
+++ src/Tablerow.js
@@ -99,8 +99,8 @@ function Tablerow() {
   })
 
   const request_getquerydata = async (body) => {
-    const base_url = 'http://localhost:8080'
-    //const base_url = 'http://ms-python-teradata-nirvana-qa.apps.ocptest.gp.inet'
+    //const base_url = 'http://localhost:8080'
+    const base_url = 'http://ms-python-teradata-nirvana-qa.apps.ocptest.gp.inet'
     const method = '/getQueryData'
     const request = {
       method: 'POST',
@@ -111,8 +111,8 @@ function Tablerow() {
   }
 
   const request_gettabledata = async (body) => {
-    const base_url = 'http://localhost:8080'
-    //const base_url = 'http://ms-python-teradata-nirvana-qa.apps.ocptest.gp.inet'
+    //const base_url = 'http://localhost:8080'
+    const base_url = 'http://ms-python-teradata-nirvana-qa.apps.ocptest.gp.inet'
     const method = '/getTableData2'
     const request = {
       method: 'POST',
